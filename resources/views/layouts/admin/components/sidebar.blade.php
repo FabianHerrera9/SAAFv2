08ed6f84c5e832abcde53686b00a182c1ec3e81e
Fabian Herrera <fherrera467@misena.edu.co>
2021-02-24 21:03:11 -0500

    cambio vista -fabian

--- resources/views/layouts/admin/components/sidebar.blade.php
+++ resources/views/layouts/admin/components/sidebar.blade.php
@@ -90,7 +90,7 @@
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="bx bx-wrench"></i>
-                        <span key="t-layouts">Mantenimiento</span>
+                        <span key="t-layouts">Mantenimiento / Garantias</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
                         <li><a href="{{route('usuarios.index')}}" key="t-horizontal" class="bx bx-search"> Listado</a></li>
