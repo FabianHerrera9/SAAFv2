747f31057953aef370d61c7e9961fa7dd8d8094c
juan2908 <51841416+juan2908@users.noreply.github.com>
2021-03-02 22:33:31 -0500

    ajuste web
    juan david

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\autn\logincontroller;
 use App\Http\Controllers\ProveedorController;
 use App\Http\Controllers\ActivoController;
 use App\Http\Controllers\DocenteController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,35 +17,42 @@ use App\Http\Controllers\DocenteController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::redirect('/','login');
-Route::get('login',[logincontroller::class,'from_login'])->name('autn.from_login');
-Route::post('autn',[logincontroller::class,'autn'])->name('autn.autn');
+Route::redirect('/', 'login');
+//Route::get('login',[logincontroller::class,'from_login'])->name('autn.from_login');
+//Route::post('autn',[logincontroller::class,'autn'])->name('autn.autn');
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
 //Usuarios
-Route::get('usuarios',[UsuariosController::class,'index'])->name('usuarios.index');
-Route::get('usuarios/crear',[UsuariosController::class,'crear'])->name('usuarios.crear');
-Route::post('usuarios',[UsuariosController::class,'guardar'])->name('usuarios.guardar');
-Route::get('usuarios/editar/{id}',[UsuariosController::class,'editar'])->name('usuarios.editar');
-Route::put('usuarios/{id}',[UsuariosController::class,'actualizar'])->name('usuarios.actualizar');
+    Route::get('usuarios', [UsuariosController::class, 'index'])->name('usuarios.index');
+    Route::get('usuarios/crear', [UsuariosController::class, 'crear'])->name('usuarios.crear');
+    Route::post('usuarios', [UsuariosController::class, 'guardar'])->name('usuarios.guardar');
+    Route::get('usuarios/editar/{id}', [UsuariosController::class, 'editar'])->name('usuarios.editar');
+    Route::put('usuarios/{id}', [UsuariosController::class, 'actualizar'])->name('usuarios.actualizar');
 
 //Proveedores
-Route::get('proveedores',[ProveedorController::class,'listar'])->name('proveedores.index');
-Route::get('proveedores/crear',[ProveedorController::class,'crear'])->name('proveedores.crear');
-Route::post('proveedores',[ProveedorController::class,'guardar'])->name('proveedores.guardar');
-Route::get('proveedores/editar/{id}',[ProveedorController::class,'editar'])->name('proveedores.editar');
-Route::put('proveedores/{id}',[ProveedorController::class,'actualizar'])->name('proveedores.actualizar');
+    Route::get('proveedores', [ProveedorController::class, 'listar'])->name('proveedores.index');
+    Route::get('proveedores/crear', [ProveedorController::class, 'crear'])->name('proveedores.crear');
+    Route::post('proveedores', [ProveedorController::class, 'guardar'])->name('proveedores.guardar');
+    Route::get('proveedores/editar/{id}', [ProveedorController::class, 'editar'])->name('proveedores.editar');
+    Route::put('proveedores/{id}', [ProveedorController::class, 'actualizar'])->name('proveedores.actualizar');
 
 //Docentes
-Route::get('docentes',[DocenteController::class,'index'])->name('docentes.index');
-Route::get('docentes/crear',[DocenteController::class,'crear'])->name('docentes.crear');
-Route::post('docentes',[DocenteController::class,'guardar'])->name('docentes.guardar');
-Route::get('docentes/editar/{id}',[DocenteController::class,'editar'])->name('docentes.edit');
-Route::put('docentes/{id}',[DocenteController::class,'actualizar'])->name('docentes.actualizar');
+    Route::get('docentes', [DocenteController::class, 'index'])->name('docentes.index');
+    Route::get('docentes/crear', [DocenteController::class, 'crear'])->name('docentes.crear');
+    Route::post('docentes', [DocenteController::class, 'guardar'])->name('docentes.guardar');
+    Route::get('docentes/editar/{id}', [DocenteController::class, 'editar'])->name('docentes.edit');
+    Route::put('docentes/{id}', [DocenteController::class, 'actualizar'])->name('docentes.actualizar');
 
 
 //activos
-Route::get('activos',[ActivoController::class,'index'])->name('activos.index');
-Route::get('activos/crear',[ActivoController::class,'crear'])->name('activos.crear');
-Route::post('activos',[ActivoController::class,'guardar'])->name('activos.guardar');
-Route::get('activos/editar',[ActivoController::class,'editar'])->name('activos.editar');
+    Route::get('activos', [ActivoController::class, 'index'])->name('activos.index');
+    Route::get('activos/crear', [ActivoController::class, 'crear'])->name('activos.crear');
+    Route::post('activos', [ActivoController::class, 'guardar'])->name('activos.guardar');
+    Route::get('activos/editar', [ActivoController::class, 'editar'])->name('activos.editar');
+
+});
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
